ecc060caba305d66286417b217521ac8e6efaf43
Khaled Ebdelli <khaledebdelli@cementys.com>
2020-09-18 11:33:01 +0200

    add logger middleware

---
+++ backend/middelware/logger.js
@@ -0,0 +1,14 @@
+const morgan = require('morgan');
+const chalk = require('chalk');
+
+module.exports = morgan((tokens, req, res) => [
+  chalk.hex('#ff4757').bold('🍄 --> '),
+  chalk.hex('#34ace0').bold(tokens.method(req, res)),
+  chalk.hex('#ffb142').bold(tokens.status(req, res)),
+  chalk.hex('#ff5252').bold(tokens.url(req, res)),
+  chalk.hex('#2ed573').bold(`${tokens['response-time'](req, res)} ms`),
+  // chalk.yellow(tokens['remote-addr'](req, res)),
+  chalk.hex('#fffa65').bold(`from ${tokens.referrer(req, res)}`),
+  chalk.hex('#f78fb3').bold(`@ ${tokens.date(req, res)}`),
+  // chalk.hex('#1e90ff')(tokens['user-agent'](req, res)),
+].join(' '));
